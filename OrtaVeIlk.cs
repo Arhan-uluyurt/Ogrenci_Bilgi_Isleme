104ee10e78ae75e47387321e84ad312b77bd1b11
Arhan-uluyurt <aberration00x@gmail.com>
2024-09-27 18:56:31 +0300

    Add files via upload

---
+++ OrtaVeIlk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogrenci_Bilgi
+{
+    internal class OrtaOkul : Ogrenci
+    {
+        public OrtaOkul(string adSoyad, int okulNo, string okulAdi)
+            : base(adSoyad, okulNo, OgretimDuzeyi.OrtaOkul, okulAdi)
+        {
+            
+        }
+    }
+    internal class IlkOkul : Ogrenci
+    {
+        public IlkOkul(string adSoyad, int okulNo, string okulAdi)
+            : base(adSoyad, okulNo, OgretimDuzeyi.IlkOkul, okulAdi)
+        {
+
+        }
+    }
+}
